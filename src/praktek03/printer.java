d8e16bc58bcad560b07297baaf12f658e8a7ca5b
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2018-04-07 16:05:29 +0800

    set atribut private

--- src/praktek03/printer.java
+++ src/praktek03/printer.java
@@ -3,9 +3,9 @@ package praktek03;
 
 
 public class printer {
-    String merk;
-    String warna;
-    double harga;
+    private String merk;
+    private  String warna;
+    private double harga;
     
     void menngantitinta(){
         System.out.println("merk \t: "+merk+"\n"+
