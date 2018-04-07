af98189ec1dcd5cd63197b77f4311eb00c7fc7d2
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2018-04-07 15:54:43 +0800

    instance buat atribut dan panggil method

--- src/praktek03/printeraksi.java
+++ src/praktek03/printeraksi.java
@@ -5,5 +5,12 @@ package praktek03;
 public class printeraksi {
     public static void main(String[] args) {
         
+        printer p = new printer();
+        p.merk="canon";
+        p.warna="hitam";
+        p.harga=1000;
+                
+        p.menngantitinta();
+        
     }
 }
