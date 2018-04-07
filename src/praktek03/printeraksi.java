954d58ddd7547cc46b8ca7651b39ba6e3f6b5b57
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2018-04-07 16:23:29 +0800

    menggunakan method set

--- src/praktek03/printeraksi.java
+++ src/praktek03/printeraksi.java
@@ -5,10 +5,10 @@ package praktek03;
 public class printeraksi {
     public static void main(String[] args) {
         
-        printer p = new printer();
-        p.merk="canon";
-        p.warna="hitam";
-        p.harga=1000;
+        printer p = new printer();         
+        p.setMerk("canon");
+        p.setWarna("hitam");
+        p.setHarga(1000);
                 
         p.menngantitinta();
         
