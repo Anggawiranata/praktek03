7ab04fa0b7447072a1ed30ff478481bba25af168
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2018-04-07 16:39:19 +0800

    atribut dan method

--- src/praktek03/printerwifi.java
+++ src/praktek03/printerwifi.java
@@ -3,5 +3,12 @@ package praktek03;
 
 
 public class printerwifi extends printer {
+    private String jenisprinter;
     
+    @Override
+    void menngantitinta(){
+               System.out.println("KRIIIK....");
+   
+
+    }
 }
