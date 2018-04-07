20579ced3226ae5fb1898dd8800a7d8d40c6122a
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2018-04-07 16:29:23 +0800

    menggunakan method get

--- src/praktek03/printeraksi.java
+++ src/praktek03/printeraksi.java
@@ -5,12 +5,19 @@ package praktek03;
 public class printeraksi {
     public static void main(String[] args) {
         
-        printer p = new printer();         
-        p.setMerk("canon");
-        p.setWarna("hitam");
+        printer p = new printer(); 
+        
+        p.setMerk("epson");
+        p.setWarna("putih");
         p.setHarga(1000);
                 
         p.menngantitinta();
         
+        System.out.print("merknya \t: ");
+        System.out.println(p.getMerk());
+        System.out.print("warnanya \t: ");
+        System.out.println(p.getWarna());
+        System.out.print("harganya \t: ");
+        System.out.println(p.getHarga());
     }
 }
